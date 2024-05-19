66b15ab2921cd9327262f279da0a4816b89fd55b
이관학 <green6294@gmail.com>
2024-05-19 23:37:25 +0900

    strndup 함수 오류 해결
    C 표준 라이브러리 구현에서 제공되지 않는 strndup 함수에 대해 코드 내에서 직접 구현

--- wordquiz-main/wordbooks/wordquiz.c
+++ wordquiz-main/wordbooks/wordquiz.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> // Include the header file for 'strndup' function
 #include <dirent.h>
 #include <ctype.h>
 
+
+#ifndef HAVE_STRNDUP
+// Implementation of strndup if not available
+char *strndup(const char *s, size_t n) {
+    char *p = (char *)malloc(n + 1);
+    if (p) {
+        strncpy(p, s, n);
+        p[n] = '\0';
+    }
+    return p;
+}
+#endif
 
 typedef 
 	enum {
@@ -15,50 +27,52 @@ typedef
 	}
 	command_t ;
 
+
 char * read_a_line (FILE * fp)
 {
-	static char buf[BUFSIZ] ;
-	static int buf_n = 0 ;
-	static int curr = 0 ;
+    static char buf[BUFSIZ] ;
+    static int buf_n = 0 ;
+    static int curr = 0 ;
 
-	if (feof(fp) && curr == buf_n - 1)
-		return 0x0 ;
+    if (feof(fp) && curr == buf_n - 1)
+        return 0x0 ;
 
-	char * s = 0x0 ;
-	size_t s_len = 0 ;
-	do {
-		int end = curr ;
-		while (!(end >= buf_n || !iscntrl(buf[end]))) {
-			end++ ;
-		}
-		if (curr < end && s != 0x0) {
-			curr = end ;
-			break ;
-		}
-		curr = end ;
-		while (!(end >= buf_n || iscntrl(buf[end]))) {
-			end++ ;
-		}
-		if (curr < end) {
-			if (s == 0x0) {
-				s = strndup(buf + curr, end - curr) ;
-				s_len = end - curr ;
-			}
-			else {
-				s = realloc(s, s_len + end - curr + 1) ;
-				s = strncat(s, buf + curr, end - curr) ;
-				s_len = s_len + end - curr ;
-			}
-		}
-		if (end < buf_n) {
-			curr = end + 1 ;
-			break ;
-		}
+    char * s = 0x0 ;
+    size_t s_len = 0 ;
+    do {
+        int end = curr ;
+        while (!(end >= buf_n || !iscntrl(buf[end]))) {
+            end++ ;
+        }
+        if (curr < end && s != 0x0) {
+            curr = end ;
+            break ;
+        }
+        curr = end ;
+        while (!(end >= buf_n || iscntrl(buf[end]))) {
+            end++ ;
+        }
+        if (curr < end) {
+            if (s == 0x0) {
+                
+                s = strndup(buf + curr, end - curr) ;
+                s_len = end - curr ;
+            }
+            else {
+                s = realloc(s, s_len + end - curr + 1) ;
+                s = strncat(s, buf + curr, end - curr) ;
+                s_len = s_len + end - curr ;
+            }
+        }
+        if (end < buf_n) {
+            curr = end + 1 ;
+            break ;
+        }
 
-		buf_n = fread(buf, 1, sizeof(buf), fp) ;
-		curr = 0 ;
-	} while (buf_n > 0) ;
-	return s ;
+        buf_n = fread(buf, 1, sizeof(buf), fp) ;
+        curr = 0 ;
+    } while (buf_n > 0) ;
+    return s ;
 }
 
 
@@ -215,7 +229,7 @@ int main ()
 
 	return EXIT_SUCCESS ;
 
-	prinf("English for Software Developer");
-	prinf("2023070002_leeyesol, 2022078069_leekwanhak");
+	printf("English for Software Developer");
+	printf("2023070002_leeyesol, 2022078069_leekwanhak");
 	
 }
